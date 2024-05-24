34a7f752cbf91e427bfa17af37b26ab422809cee
Adriano Silva <lucio.adriano@gmail.com>
2024-05-24 14:00:50 -0300

    css nos componentes de layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { NewMomentComponent } from './pages/new-moment/new-moment.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'moment/new', component: NewMomentComponent},
+    {path: 'about', component: AboutComponent},
+];
