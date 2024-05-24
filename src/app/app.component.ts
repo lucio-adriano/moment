9a3f413641aa392ea8ec39000446f9bf986a29fa
Adriano Silva <lucio.adriano@gmail.com>
2024-05-24 17:49:18 -0300

    Pagina de formulario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,15 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 import { FooterComponent } from './components/footer/footer.component';
 
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, FooterComponent, HeaderComponent]
+    imports: [RouterOutlet, FooterComponent, HeaderComponent, HttpClientModule, FormsModule, ReactiveFormsModule]
 })
 export class AppComponent {
   title = 'moments';
--- src/app/pages/new-moment/new-moment.component.ts
+++ src/app/pages/new-moment/new-moment.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { MomentFormComponent } from '../../components/moment-form/moment-form.component';
 
 @Component({
   selector: 'app-new-moment',
   standalone: true,
-  imports: [],
+  imports: [MomentFormComponent],
   templateUrl: './new-moment.component.html',
   styleUrl: './new-moment.component.css'
 })
 export class NewMomentComponent {
+  btnText = "Compatilhar!";
 
 }
