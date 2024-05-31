5419bea6edc9b2df52feca458ab026b030aae5eb
Adriano Silva <lucio.adriano@gmail.com>
2024-05-31 16:41:32 -0300

    busca de dados

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -27,19 +27,27 @@ export class HomeComponent {
   allMoments: Moment[] = [];
   moments: Moment[] = [];
   baseApiUrl = environment.baseApiUrl;
+  faSearch = faSearch;
+  searchTerm: string = '';
 
-  constructor(private momentService: MomentService) { 
+  constructor(private momentService: MomentService) {
 
     this.momentService.getMoments().subscribe((items: any) => {
       const data = items.data
-  
+
       data.map((item: any) => {
         item.created_at = new Date(item.created_at).toLocaleDateString('pt-BR');
       });
-  
+
       this.allMoments = data;
       this.moments = data;
-  
+
     })
+  }
+  search(e: Event): void {
+    const target = e.target as HTMLInputElement;
+    const value = target.value;
+
+    this.moments = this.allMoments.filter((moment) => moment.title?.toLocaleLowerCase().includes(value))
   }
 }
