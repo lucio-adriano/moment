5c07518be51e884930a914cfc2f6218abca98df5
Adriano Silva <lucio.adriano@gmail.com>
2024-06-03 16:13:01 -0300

    css da pagina moment

--- src/app/pages/moment/moment.component.ts
+++ src/app/pages/moment/moment.component.ts
@@ -1,19 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, EnvironmentInjector } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { MomentService } from '../../services/moment.service';
+import { faEdit, faTimes } from '@fortawesome/free-solid-svg-icons';
 
 import { Moment } from '../../entities/moment';
 import { ActivatedRoute } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
+import { environment } from '../../../environments/environment';
+
+
 @Component({
   selector: 'app-moment',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FontAwesomeModule],
   templateUrl: './moment.component.html',
   styleUrl: './moment.component.css'
 })
 export class MomentComponent {
+  faEdit = faEdit;
+  faTimes= faTimes;
+  baseApiUrl = environment.baseApiUrl;
 
   moment: Moment | undefined;
 
